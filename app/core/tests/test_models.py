db4a516c0fdf8ebe9a58c022279f1426d551e92e
Yaroslav <ppdraga@gmail.com>
2020-08-24 10:57:25 +0300

    email test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,3 +2,21 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'ppdraga@mail.ru'
+        password = 'password123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'ppdraga@Mail.ru'
+        user = get_user_model().objects.create_user(email, 'password123')
+
+        self.assertEqual(user.email, email.lower())
