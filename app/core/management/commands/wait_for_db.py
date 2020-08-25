fba07616a370a8a214ad2579ba57b2a85198c8a5
Yaroslav <ppdraga@gmail.com>
2020-08-25 11:59:15 +0300

    wait for db test 1

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+# import time
+
+# from django.db import connections
+# from django.db.utils import OperationalError
+# from django.core.management.base import BaseCommand
+
+
+# class Command(BaseCommand):
+#     pass
