9f87c2690d906ba2f2eb95d2bcba2c5dc10274c5
Yaroslav <ppdraga@gmail.com>
2020-08-26 16:47:20 +0300

    drf 0

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,9 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+
