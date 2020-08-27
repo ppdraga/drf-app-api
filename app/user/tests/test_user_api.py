0213d3ffaaccc9ad5eadc8a14afbb24ac71a1bce
Yaroslav <ppdraga@gmail.com>
2020-08-27 11:25:05 +0300

    auth token api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,10 +7,11 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
-    return get_user_model().objects.create(**params)
+    return get_user_model().objects.create_user(**params)
 
 
 class PublicUserApiTests(TestCase):
@@ -51,3 +52,41 @@ class PublicUserApiTests(TestCase):
         user_exists = get_user_model().objects\
             .filter(email=payload['email']).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        payload = {
+            'email': 'test1@test.com',
+            'password': 'test1passwd',
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('token', res.data)
+
+    def test_create_token_invalid_credentials(self):
+        create_user(email='test@test.com', password='testpasswd')
+        payload = {
+            'email': 'test@test.com',
+            'password': 'invalid_passwd'
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_for_no_user(self):
+        payload = {
+            'email': 'test@test.com',
+            'password': 'testpasswd'
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        payload = {
+            'email': 'test',
+            'password': ''
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,17 @@
 # from django.shortcuts import render
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer, AuthTokenSerialized
 
 # Create your views here.
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerialized
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
