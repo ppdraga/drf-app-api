a9c2e4999313eca9965c590f1aeca06b32d99509
Yaroslav <ppdraga@mail.ru>
2020-08-27 23:09:45 +0300

    error

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -15,3 +15,13 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerialized
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class=UserSerializer
+    authentication_classes=(authentication.TokenAuthentication,)
+    permission_classes=(permissions.IsAuthenticated,)
+    queryset=''
+
+    def get_user(self):
+        return self.request.user
