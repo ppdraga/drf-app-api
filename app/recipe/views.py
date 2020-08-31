ba7414b102bfda7b3badd3f340685b2c93c29a78
Yaroslav <ppdraga@gmail.com>
2020-08-31 09:53:54 +0300

    api create tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,11 +6,17 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     serializer_class = serializers.TagSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
 
     def get_queryset(self):
+        '''Return objects for the current authenticated user only'''
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        '''Create new tag'''
+        serializer.save(user=self.request.user)
